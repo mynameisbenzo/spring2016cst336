8808fd1c089d35f4b0e6a910dc8c4a07d6dc070e
mynameisbenzo <my_name.is_benzo@yahoo.com>
2016-02-24 19:29:38 +0000

    added cards in index

---
+++ game.php
@@ -0,0 +1,83 @@
+<?php 
+
+
+    $card = ["image" => "", 
+            "score" => "",
+            "suit" => "",
+            "rank" => "" ];
+    
+    //indexed array
+    $deck = ["ClubA","Club1","Club2","Club3","Club4","Club5","Club6","Club7","Club8","Club9","Club10","Club11","Club12","Club13",
+             "DiamondA","Diamond1","Diamond2","Diamond3","Diamond4","Diamond5","Diamond6","Diamond7","Diamond8","Diamond9","Diamond10","Diamond11","Diamond12","Diamond13",
+             "HeartA","Heart1","Heart2","Heart3","Heart4","Heart5","Heart6","Heart7","Heart8","Heart9","Heart10","Heart11","Heart12","Heart13",
+             "SpadeA","Spade1","Spade2","Spade3","Spade4","Spade5","Spade6","Spade7","Spade8","Spade9","Spade10","Spade11","Spade12","Spade13"];
+    
+    
+    
+    shuffle($deck);
+    
+    var_dump($deck);
+    
+    
+    
+    /*
+    for($i = 0; $i < 52; $i++)
+    {
+        if($i < 13)
+        {
+            
+        }
+        if()
+        if()
+        if()
+    }
+    */
+    
+    //associative array
+    $player1 = ["imageName" => "",
+                "name" => $_POST["p1"] ];
+    $player2 = ["imageName" => "",
+                "name" => $_POST["p2"] ];
+    $player3 = ["imageName" => "",
+                "name" => $_POST["p3"] ];
+    $player4 = ["imageName" => "",
+                "name" => $_POST["p4"] ];
+                
+    $table = [$player1, $player2, $player3, $player4];
+    
+    var_dump($table);
+              
+    $hand = ["player" => null, 
+             "cards" => [],
+             "score" => "" ];
+    
+    $game = ["location" => $table, 
+             "hands"=> $hand];
+    
+    ?>
+    
+<html>
+
+<head>
+    <title> </title>
+</head>
+
+<body>
+    
+    <?php
+    
+    
+    
+    ?>
+
+    <form action  = "game.php" method = "POST">
+        <input type = "hidden" value = "<?= $_POST["p1"] ?>" />
+        <input type = "hidden" value = "<?= $_POST["p2"] ?>" />
+        <input type = "hidden" value = "<?= $_POST["p3"] ?>" />
+        <input type = "hidden" value = "<?= $_POST["p4"] ?>" />
+        <input type = "submit" value = "Play again!" />
+    </form>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <form action = "game.php" method ="POST">
+            <div>
+                <label>Player 1:</label><input type = "text" name = "p1"/>
+            </div>
+            <div>
+                <label>Player 2:</label><input type = "text" name = "p2"/>
+            </div>
+            <div>
+                <label>Player 3:</label><input type = "text" name = "p3"/>
+            </div>
+            <div>
+                <label>Player 4:</label><input type = "text" name = "p4"/>
+            </div>
+            
+            <input type = "submit" value = "Go!"/>
+            <input type = "clear" value = "Start Over!"/>
+        </form>
+
+    </body>
+</html>
+
+<?php
+
+?>
